b3857b47b956446469e2c370d10bd351afc3583f
Tatiana Toro Giraldo <tatianatorog@gmail.com>
2023-12-26 12:04:58 -0500

    set up proyect, queries and schema

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+//types
+import { typeDefs } from './schema.js';
 
-// server set
+// server setup 
 const server = new ApolloServer({ typeDefs, resolvers });
 
+// The `listen` method launches a web server.
 const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
